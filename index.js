6201746aa93922de12c4ca3d299f6a5602c6e735
Troy Chryssos <github email address>
2016-09-21 17:15:24 -0400

    used map to create new arrays of sorted or modified data

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+let sortedRobots = robots.map(function(robot){
+  let newBot=Object.assign({},robot);
+
+  if (knownDecepticons.includes(robot.name)){
+    newBot.alliance="decepticon"
+  } else {
+    newBot.alliance="autobot"
+  }
+  return newBot
+})
+
+let coloredZebraStripes=zebraStripes.map(function(stripe, index){
+  let newStripe=Object.assign({}, stripe)
+  if (index % 2 === 0 || index === 0){
+    newStripe.color="black"
+  } else {
+    newStripe.color="white"
+  }
+  return newStripe
+})
